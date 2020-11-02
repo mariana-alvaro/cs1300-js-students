a452a6313a0576909586f703c9d4cd8c0345d67f
Mariana Alvaro <mariana_alvaro@brown.edu>
2020-11-02 14:04:05 -0500

    updates

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var corsApiUrl = "https://cors-anywhere.herokuapp.com/";
 // TODO: REPLACE YOUR TOKEN
-var apiToken = "?token=YOUR_TOKEN_HERE";
+var apiToken = "?token=OwJvyDhf5jRKA80LxcYcyvgxgTDgtkbBuKBqbbMLC5E";
 
 // CORS stands for "cross origin resource sharing" -- you'll be making http requests in order
 // DON'T CHANGE THIS: fetches the data from the API endpoint
@@ -24,8 +24,32 @@ const corsPromise = () =>
 corsPromise().then(
   (request) =>
     (request.onload = request.onerror = function () {
-      // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
-    })
+      handleResponse(request.response);
+      })
 );
 
 //// TODO: ADD WHATEVER FUN CONTENT YOU WANT ////
+
+const handleResponse = (response) => {
+  const parsed_json = JSON.parse(response);
+  const plants = parsed_json.data;
+  console.log(plants);
+
+  for (const plant of plants) {
+    makeHTML(plant);
+  }
+}
+
+const makeHTML = (plant) => {
+
+  const plantDiv = document.createElement("div"); 
+  plantDiv.innerText = plant.common_name;
+  const commonNameDiv = document.getElementById("common-name");
+  commonNameDiv.appendChild(plantDiv);
+
+  const plantDiv2 = document.createElement("div"); 
+  plantDiv2.innerText = plant.scientific_name;
+  const sciNameDiv = document.getElementById("sci-name");
+  sciNameDiv.appendChild(plantDiv2);
+}
+
